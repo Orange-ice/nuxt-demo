095634e2a77e48979103f7611d882ee3d51da97b
Burt <2902978956@qq.com>
2022-06-27 21:06:26 +0800

    init project

---
+++ pages/about.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <nuxt-link to="/">
+      首页
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutPage'
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h3>Home</h3>
+    <nuxt-link to="/about">
+      About
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
